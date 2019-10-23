9786650a923b96242fb68b976e9f7f276dac6692
Jacob Redmon <47927946+jacobredmon@users.noreply.github.com>
2019-10-23 15:18:58 -0400

    Update Errorlistener.java

--- Errorlistener.java
+++ Errorlistener.java
@@ -19,11 +19,7 @@ public class Errorlistener extends BaseErrorListener{
 
 	public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e){
 
-		List<String> stack = ((Parser) recognizer).getRuleInvocationStack();
-		Collections.reverse(stack);
 		System.out.println("An error was encountered at line " + line + ", col " + charPositionInLine);
-		//System.err.println("rule stack "+stack);
-		//System.err.println("Line " + line + ": " + charPositionInLine+" at" + offendingSymbol + ": " + msg);
 		System.exit(1);
 		
 	}// end method
