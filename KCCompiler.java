aeaf12f3a3662f53e31343c83af459b343f070c9
Jacob Redmon <47927946+jacobredmon@users.noreply.github.com>
2019-10-23 10:26:20 -0400

    Create KCCompiler.java

---
+++ KCCompiler.java
@@ -0,0 +1,48 @@
+/**
+* This is the compiler for the KnightCode programming language.
+* @author Max Bickley
+* @author Chad Critchelow
+* @author Jacob Redmon
+* @author Ben Torrance
+* @version 1.0
+* Programming Project Two
+* CS322 - Compiler Construction
+* Fall 2019
+**/
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class KCCompiler {
+
+    public static void main(String[] args) throws Exception {
+    
+        String inputFile = null; 
+        if ( args.length>0 ) 
+		    inputFile = args[0];
+          
+        InputStream is = System.in;
+        if ( inputFile!=null ) 
+		    is = new FileInputStream(inputFile);
+        
+        ANTLRInputStream input = new ANTLRInputStream(is); 
+        tinybasicLexer lexer = new tinybasicLexer(input); 
+        CommonTokenStream tokens = new CommonTokenStream(lexer); 
+        tinybasicParser parser = new tinybasicParser(tokens); 
+        
+	    parser.removeErrorListeners();
+
+	    parser.addErrorListener(new ErrorListener());
+
+	    ParseTree tree = parser.program(); // start at the program label
+
+	    ParseTreeWalker walker = new ParseTreeWalker();
+		
+	    MyListener l = new MyListener();
+	    walker.walk(l, tree);
+
+	}//end main
+  
+}//end class
