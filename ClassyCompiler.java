e1842b9ed22a7abfe0df2fedc7ade6947d09f52f
Maxwell-Bickley <55039725+Maxwell-Bickley@users.noreply.github.com>
2019-10-23 16:59:42 -0400

    Update ClassyCompiler.java

--- ClassyCompiler.java
+++ ClassyCompiler.java
@@ -11,70 +11,71 @@ import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import static org.antlr.v4.runtime.CharStreams.fromFileName;
 
-public class ClassyCompiler{
 
-        public static String Userchoice;
-        public static Mylistener Mylistener = new Mylistener();
-        public static HashMap<Integer, String> parsed = Mylistener.sender();
-        public static Integer Part = 0;
-        public static String Decision;
-        public static String convert1;
-        public static String convert2;
-        public static String message;
-        public static String Outfile;
+public class ClassyCompiler
+{
+	public static HashMap<Integer, String> Userchoice = new HashMap<Integer, String>();
+	public static Mylistener Mylistener = new Mylistener();
+	public static HashMap<Integer, String> parsed = Mylistener.sender();
+	public static Integer Part = 0;
+	public static String Decision;
+	public static String convert1;
+	public static String convert2;
+	public static String message;
+	public static String Outfile;
+	public static void main(String[] args) throws Exception
+				{
+				Userchoice.put( 0 , args[0]);
+				Userchoice.put( 1 , args[1]);
+				Userchoice.put( 2 , args[2]);
 
-        public static void main(String[] args) throws Exception{
-                Userchoice = args[2];
-                String line = null;
-                if (args[0] == "source"){
-                        //Takes the input filename from the arguments array and creates a File object using that name as the path.
-                        String FilePath = args[1];
-                        File FileName = new File(FilePath);
+                	String line = null;
+			if (args[1] != null)
+                	{	
+			String FilePath = args[1];
+                	File FileName = new File(FilePath);
+			Outfile = args[2];
 
-                        Outfile = args[2];
+			CharStream PROGRAM = fromFileName(FilePath);  //load the file	
+			knightcodeLexer lexer = new knightcodeLexer(PROGRAM);
+			lexer.removeErrorListeners();
+			lexer.addErrorListener(new Errorlistener());
 
-                        CharStream PROGRAM = fromFileName(FilePath);  //load the file and enters the character stream into the lexer.
-                        knightcodeLexer lexer = new knightcodeLexer(PROGRAM);
+			CommonTokenStream tokens = new CommonTokenStream(lexer); //scan stream for tokens
+			knightcodeParser parser = new knightcodeParser(tokens);  //parse the tokens
+			
+			parser.removeErrorListeners();
+			parser.addErrorListener(new Errorlistener());
+			ParseTree tree = parser.file();//parse the content and get the tree
 
-                        lexer.removeErrorListeners();//Cleans up any error listeners and then creates a new error listener for the lexer
-                        lexer.addErrorListener(new Errorlistener());
+			Mylistener listener = new Mylistener();
 
-                        CommonTokenStream tokens = new CommonTokenStream(lexer); //scan stream for tokens
-                        knightcodeParser parser = new knightcodeParser(tokens);  //parse the tokens
-
-                        parser.removeErrorListeners();//Cleans up any remaining error listeners and then creates a fresh error listener for the parser
-                        parser.addErrorListener(new Errorlistener());
-
-                        ParseTree tree = parser.file();//parse the content and get the tree
-
-                        Mylistener listener = new Mylistener();
-
-                        ParseTreeWalker walker = new ParseTreeWalker();
-                        walker.walk(listener,tree);
-
-                        if (!Outfile.equals("")){
-                                File newfile = new File (Outfile);
-                                System.out.println("Creating " + newfile + "...");
-                                newfile.createNewFile();
-                                FileOutputStream writing = new FileOutputStream(newfile);
-
-                                for (int writer = 0; writer < parsed.size(); writer = writer + 1){
-                                        convert1 = (String) parsed.get(writer);
-                                        writing.write(convert1.getBytes());
-                                        writing.write(System.getProperty("line.separator").getBytes());
-                                }
-                                writing.flush();
-                                writing.close();
-                        }
-                }
-                if (args[0] == "bytecode"){
-                        System.out.println("bytecode output has not yet been implemented");
-                }
-                else {
-                        System.out.println("You were supposed to enter: java, ClassyCompiler, source, the file you want to read, and the file you want to create.");
-                }
-        }
-        public static String sender(){
-                return Userchoice;
-        }
+			ParseTreeWalker walker = new ParseTreeWalker();
+			walker.walk(listener,tree);
+		if (!Outfile.equals(""))
+		{
+		File newfile = new File (Outfile);
+					System.out.println("Creating " + newfile + "...");
+					newfile.createNewFile();
+					FileOutputStream writing = new FileOutputStream(newfile);
+		
+					for (int writer = 0; writer < parsed.size(); writer = writer + 1)
+					{
+						convert1 = (String) parsed.get(writer);
+						writing.write(convert1.getBytes());
+						writing.write(System.getProperty("line.separator").getBytes());
+					}
+					writing.flush();
+					writing.close();
+		}
+		}
+		else 
+		{
+		System.out.println("You were supposed to enter: java, ClassyCompiler, source, the file you want to read, and the file you want to create.");
+		}
+	}
+	public HashMap<Integer,String> sender()
+        {
+        return Userchoice;
+        }	
 }
